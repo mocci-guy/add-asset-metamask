846d52d2044d7776473cd436eee8cc3666c562d2
hamburger <tik.shitting@gmail.com>
2022-12-30 22:20:42 +0200

    second commit

--- script.js
+++ script.js
@@ -3,4 +3,57 @@ const ethereumButton = document.querySelector('.enableEthereumButton');
 ethereumButton.addEventListener('click', () => {
   //Will Start the metamask extension
   ethereum.request({ method: 'eth_requestAccounts' });
+  try {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x38' }],   // BCS CHAIN
+    });
+  } catch (switchError) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (switchError.code === 4902) {
+      try {
+        await ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: '0x38',
+              chainName: 'Smart Chain',
+              rpcUrls: ['https://bsc-dataseed.binance.org/'] /* ... */,
+            },
+          ],
+        });
+      } catch (addError) {
+        // handle "add" error
+      }
+    }
+    // handle other "switch" errors
+  }
+  const tokenAddress = '0xd92377433705E3065f1706d618eBE043bDA751c4';
+  const tokenSymbol = 'SHTC';
+  const tokenDecimals = 18;
+   const tokenImage = 'https://yawgoo87.github.io/express-vercel/metamask-2728406-2261817-3056404914.png';
+
+  try {
+  // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+  const wasAdded = await ethereum.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20', // Initially only supports ERC20, but eventually more!
+      options: {
+        address: tokenAddress, // The address that the token is at.
+        symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+        decimals: tokenDecimals, // The number of decimals in the token
+        image: tokenImage, // A string url of the token logo
+      },
+    },
+  });
+
+  if (wasAdded) {
+    console.log('Thanks for your interest!');
+  } else {
+    console.log('Your loss!');
+  }
+  } catch (error) {
+  console.log(error);
+}
 });
